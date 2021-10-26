ff848287a502729b40aadca5b20338a47cf81255
tms-ninja <65960785+tms-ninja@users.noreply.github.com>
2021-10-26 11:31:48 +0100

    Event stores collision type and index of second obejct
    - Event now stores what type of collision occurred (disc-disc or disc-wall) and the index of other disc or wall involved
    - Event allows default construction

--- cpp/billiards/Event.cpp
+++ cpp/billiards/Event.cpp
@@ -1,13 +1,17 @@
 #include "Event.h"
 
-Event::Event(const double t, const size_t ind, const Vec2D &pos, const Vec2D &new_v)
-	: pos{ pos }, new_v{ new_v }, t{ t }, ind{ ind }
+Event::Event()
+{
+}
+
+Event::Event(const double t, const size_t ind, const size_t second_ind, const bool disc_wall_col, const Vec2D &pos, const Vec2D &new_v)
+	: pos{ pos }, new_v{ new_v }, t{ t }, ind{ ind }, second_ind{ second_ind }, disc_wall_col{ disc_wall_col }
 {
 }
 
 std::ostream & operator<<(std::ostream & os, const Event & e)
 {
-	std::cout << e.t << '\t' << e.ind << '\t' << e.pos << '\t' << e.new_v;
+	std::cout << e.t << '\t' << e.ind << '\t' << e.second_ind << '\t' << e.disc_wall_col << '\t' << e.pos << '\t' << e.new_v;
 
 	return os;
 }
--- cpp/billiards/Event.h
+++ cpp/billiards/Event.h
@@ -8,9 +8,13 @@ public:
 	Vec2D pos;
 	Vec2D new_v;
 	double t;
-	size_t ind;
+	size_t ind;			 // Index of the disc involved in the event
+	size_t second_ind;   // index of the other thing involved in the event
+	bool disc_wall_col;  // true if event is a disc-wall collision
 
-	Event(const double t, const size_t ind, const Vec2D &pos, const Vec2D &new_v);
+	Event();
+
+	Event(const double t, const size_t ind, const size_t second_ind, const bool disc_wall_col, const Vec2D &pos, const Vec2D &new_v);
 };
 
 std::ostream& operator<<(std::ostream &os, const Event &e);
