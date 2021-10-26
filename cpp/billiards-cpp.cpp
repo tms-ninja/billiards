67326bc0b2da56d8048bd5ab3ccf60de622afbef
tms-ninja <65960785+tms-ninja@users.noreply.github.com>
2021-10-26 12:00:47 +0100

    Update billiards-cpp.cpp

--- cpp/billiards-cpp.cpp
+++ cpp/billiards-cpp.cpp
@@ -2,8 +2,22 @@
 //
 
 #include <iostream>
+#include "billiards/Sim.h"
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	Sim s;
+
+	s.walls.push_back(Wall{ { 0.0,  -10.0 }, {  0.0,  10.0 } });
+	s.walls.push_back(Wall{ { 0.0,   10.0 }, { 10.0,  10.0 } });
+	s.walls.push_back(Wall{ { 10.0,  10.0 }, { 10.0, -10.0 } });
+	s.walls.push_back(Wall{ { 10.0, -10.0 }, { 0.0,  -10.0 } });
+
+	s.current_state.push_back(Disc{ {3.0, 0.0}, {1.0, 0.0}, 1.0, 1.0 });
+	s.current_state.push_back(Disc{ {7.0, sqrt(2)}, {-1.0, 0.0}, 1.0, 1.0 });
+
+	s.advance(1, 100.0, true);
+
+	for (Event& e : s.events)
+		std::cout << e << '\n';
 }
