c7c0fb5ea60f3efb0ee4d48be6c2f353d25d9111
tms-ninja <65960785+tms-ninja@users.noreply.github.com>
2021-11-08 12:17:29 +0000

    Add performance test
    - Add simple simulation of 100 discs to benchmark improvements from better algorithm

--- cpp/billiards-cpp.cpp
+++ cpp/billiards-cpp.cpp
@@ -2,22 +2,57 @@
 //
 
 #include <iostream>
+#include <chrono>
+
 #include "billiards/Sim.h"
 
-int main()
+// For testing performance improvements are actually improvements
+Sim setup_perf_test()
 {
 	Sim s;
 
-	s.walls.push_back(Wall{ { 0.0,  -10.0 }, {  0.0,  10.0 } });
+	s.walls.push_back(Wall{ { 0.0,    0.0 }, {  0.0,  10.0 } });
 	s.walls.push_back(Wall{ { 0.0,   10.0 }, { 10.0,  10.0 } });
-	s.walls.push_back(Wall{ { 10.0,  10.0 }, { 10.0, -10.0 } });
-	s.walls.push_back(Wall{ { 10.0, -10.0 }, { 0.0,  -10.0 } });
+	s.walls.push_back(Wall{ { 10.0,  10.0 }, { 10.0,   0.0 } });
+	s.walls.push_back(Wall{ { 10.0,   0.0 }, { 0.0,    0.0 } });
 
-	s.current_state.push_back(Disc{ {3.0, 0.0}, {1.0, 0.0}, 1.0, 1.0 });
-	s.current_state.push_back(Disc{ {7.0, sqrt(2)}, {-1.0, 0.0}, 1.0, 1.0 });
+	Vec2D pos, v;
+	double m{ 1.0 }, R{ 0.4 };
+	double L{ 10.0 };  // width of box
 
-	s.advance(1, 100.0, true);
+	// Add balls on a 10 by 10 grid
+	for (size_t i = 0; i < 10; ++i)
+	{
+		for (size_t j = 0; j < 10; ++j)
+		{
+			pos = { i + 0.5, j + 0.5 };
 
-	for (Event& e : s.events)
-		std::cout << e << '\n';
+			v = { static_cast<double>((i * 17 + j) % 7), static_cast<double>((j * 13 + i) % 11) };
+			v /= v.mag();
+
+			s.current_state.emplace_back(pos, v, m, R);
+		}
+	}
+
+	return s;
+}
+
+// Deliberately take it by copy
+void run_perf_test(Sim s)
+{
+	s.advance(40000, 10000.0, true);
+}
+
+int main()
+{
+
+	Sim s{ setup_perf_test() };
+
+	auto begin = std::chrono::steady_clock::now();
+
+	run_perf_test(s);
+
+	auto end = std::chrono::steady_clock::now();
+
+	std::cout << "Total duration: " << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() / 1000 << "[ms]" << std::endl;
 }
