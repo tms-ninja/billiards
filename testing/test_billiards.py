0c5d90c1fa11a1313e43247b6d255ad91bbe3e01
tms-ninja <65960785+tms-ninja@users.noreply.github.com>
2021-11-27 16:05:40 +0000

    Add simulation based tests
    - Tests use a 20 disc simulation
    - Test kinetic energy of discs is conserved
    - Test no disc goes out of bounds
    - Test no discs overlap

--- testing/test_billiards.py
+++ testing/test_billiards.py
@@ -5,6 +5,34 @@ from numpy.testing import assert_allclose
 
 import billiards as bl
 
+def total_KE(m, v):
+    ke = m * np.linalg.norm(v, axis=1)**2
+
+    return np.sum(ke) / 2.0
+
+def create_20_disc_sim():
+    """
+    Creates and runs a simulation containing 20 discs for verification pruposes
+    """
+
+    # Set the seed so we know add_random_discs() won't fail
+    np.random.seed(30)
+
+    s = bl.PySim()
+
+    box_bottom_left = [ 0.0,  0.0]
+    box_top_right = [20.0, 20.0]
+
+    s.add_box_walls(box_bottom_left, box_top_right)
+    s.add_random_discs(np.array(box_bottom_left), np.array(box_top_right), 20, 5.0, 1.0, 1.0)
+
+    s.setup()
+
+    # Perform some collision
+    s.advance(1000, 100.0, True)
+
+    return {'box': [box_bottom_left, box_top_right], 's': s}
+
 
 class Test_PySim(unittest.TestCase):
     
@@ -341,3 +369,72 @@ class Test_PySim(unittest.TestCase):
             assert_allclose(ev.pos, exp['pos'])
             assert_allclose(ev.new_v, exp['new_v'])
     
+    def test_KE_conservation(self):
+        """
+        Tests kinetic energy is approximately conserved during a 
+        test simulation
+        """
+        
+        sim = create_20_disc_sim()
+
+        s = sim['s']
+
+        initial_state = s.initial_state
+
+        initial_KE = total_KE(initial_state['m'], initial_state['v'])
+
+        for c_state in s.replay_by_time(0.1):
+            m, v = c_state['m'], c_state['v']
+
+            current_KE = total_KE(m, v)
+
+            self.assertAlmostEqual(current_KE, initial_KE)
+
+    def test_out_of_bounds(self):
+        """
+        Tests discs don't go out of bounds during a test simulation
+        """
+
+        sim = create_20_disc_sim()
+
+        s = sim['s']
+        box_bottom_left, box_top_right = sim['box']
+
+        left, bottom = box_bottom_left
+        right, top = box_top_right
+
+        R = s.initial_state['R']
+
+        for c_state in s.replay_by_time(0.1):
+            pos = c_state['r']
+
+            np.testing.assert_array_equal(left + R <= pos[:, 0], True)
+            np.testing.assert_array_equal(pos[:, 0] <= right - R, True)
+
+            np.testing.assert_array_equal(bottom + R <= pos[:, 1], True)
+            np.testing.assert_array_equal(pos[:, 1] <= top - R, True)
+
+    def test_overlapping(self):
+        """
+        Tests discs don't overlap during a test simulation
+        """
+
+        sim = create_20_disc_sim()
+        s = sim['s']
+
+        R = s.initial_state['R']
+
+        for c_state in s.replay_by_time(0.1):
+            pos = c_state['r']
+
+            for i in range(pos.shape[0]):
+
+                diff = pos[i+1:] - pos[i] 
+
+                dist = np.linalg.norm(diff, axis=1)
+
+                np.testing.assert_array_equal(R[i] + R[i+1:] <= dist, True)
+
+
+
+        
