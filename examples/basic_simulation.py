90af07eb9b970c80e76f321a149adf1c9b93cd8d
tms-ninja <65960785+tms-ninja@users.noreply.github.com>
2022-07-08 15:54:03 +0100

    Tidy up comments on basic simulation example

--- examples/basic_simulation.py
+++ examples/basic_simulation.py
@@ -24,13 +24,6 @@ import billiards as bl
 print("started")
 
 # First setup the simulation
-# walls = [
-#     {'start': [ 0.0,  -10.0 ], 'end': [  0.0,  10.0 ]},
-#     {'start': [ 0.0,   10.0 ], 'end': [ 10.0,  10.0 ]},
-#     {'start': [ 10.0,  10.0 ], 'end': [ 10.0, -10.0 ]},
-#     {'start': [ 10.0, -10.0 ], 'end': [ 0.0,  -10.0 ]},
-# ]
-
 discs = [
     {'start': [3.0, 0.0], 'v': [1.0, 0.0], 'm': 1.0, 'R': 1.0},
     {'start': [7.0, np.sqrt(2.0)], 'v': [-1.0, 0.0], 'm': 4.0, 'R': 2.0},
@@ -38,23 +31,16 @@ discs = [
 
 s = bl.PySim([-10.0, -10.0], [10.0, 10.0])
 
-# for w in walls:
-#     s.add_wall(np.array(w['start']), np.array(w['end']))
-
 for d in discs:
     s.add_disc(np.array(d['start']), np.array(d['v']), d['m'], d['R'])
-
 
 # Set up the simulation ready to be run
 s.setup()
 
-
-
 # Perform one collision
 s.advance(10, 10000.0, True)
 
-print("Got this far")
-
+# Print the events that occurred
 for e in s.events:
     print(f"{e.t}\t{e.ind}\t{e.second_ind}\t{e.disc_wall_col}\t{e.pos}\t{e.new_v}")
 
