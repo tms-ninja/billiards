be56baa23aa80426f9a213bbf3061eb6f2e3f1ce
tms-ninja <65960785+tms-ninja@users.noreply.github.com>
2022-07-08 15:18:32 +0100

    Add license headers

--- cpp/billiards/Sim.cpp
+++ cpp/billiards/Sim.cpp
@@ -1,3 +1,21 @@
+// billiards: Program to model collisions between 2d discs
+// Copyright (C) 2022  Tom Spencer (tspencerprog@gmail.com)
+//
+// This file is part of billiards
+//
+// billiards is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 #include "Sim.h"
 
 Sim::Sim(Vec2D bottom_left, Vec2D top_right, size_t N, size_t M)
